579134dd4a70fb5162565b15bd53c27de05343ff
aprisun <aprisun@gmail.com>
2016-06-19 01:12:46 +0900

    createuser0619

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets, only:[:new, :create, :edit, :update]
 
-　get 'signup'  => 'members#new'
+  get 'signup'  => 'members#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+
   #resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
