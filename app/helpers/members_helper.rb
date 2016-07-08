6983781f9f2ee5fcc1d3716108f53c325825ec93
aprisun <aprisun@gmail.com>
2016-07-09 01:36:44 +0900

    admin user added

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,8 +1,9 @@
 module MembersHelper
 
-  def gravatar_for(member)
+  def gravatar_for(member, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(member.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: member.user_name, class: "gravatar")
   end
 end
