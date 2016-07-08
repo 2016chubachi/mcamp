a387b561aae86d9f41d9db66db9495c41efbba49
takeshitou <takeshitou@gmail.com>
2016-07-08 07:03:25 +0000

    update borrow item module

--- app/controllers/tenders_controller.rb
+++ app/controllers/tenders_controller.rb
@@ -5,24 +5,40 @@ class TendersController < ApplicationController
   end
 
   def show
+    #@borrow_item = BorrowItem.find(params[:id])
+    #@borrowReplies = @borrow_item.borrow_replies.find_by(member_id: current_member)
+    #if current_member && @borrow_item.borrow_state.id==1
+    #    #アイテムステータスが「募集中」の場合
+    #   @borrow_item.borrow_replies.build({member_id: current_member.id,message_state_id: 1,delete_flg: false})
+    #end
     @borrow_item = BorrowItem.find(params[:id])
-    @borrowReplies = @borrow_item.borrow_replies.find_by(member_id: current_member)
-    if current_member && @borrow_item.borrow_state.id==1
-        #アイテムステータスが「募集中」の場合
-       @borrow_item.borrow_replies.build({member_id: current_member.id,message_state_id: 1,delete_flg: false})
+    #該当ユーザーのリクエスト
+    if current_member 
+      @borrowReplies = @borrow_item.borrow_replies.where(member_id: current_member)
     end
-    
-    
+    #アイテムステータスが「募集中」の場合
+    if @borrow_item.borrow_state.id == 1
+        @newBorrowReply = BorrowReply.new
+    end
   end
   
   def update
     @borrow_item = BorrowItem.find(params[:id])
-    @borrow_item.assign_attributes(borrow_reply_params)
+    #やり方その1
+    #@newBorrowReply = BorrowReply.new(borrow_reply_params)
+    #@newBorrowReply.borrow_item = @borrow_item
+    #@newBorrowReply.member = current_member
+    #@newBorrowReply.message_state_id = 1
+    #@newBorrowReply.delete_flg = false
+    #やり方その2
+    @borrow_item.borrow_replies.build({member_id: current_member.id,message_state_id: 1,delete_flg: false}.merge(borrow_reply_params))
+    @borrow_item.borrow_state_id = 2
     if @borrow_item.save
-      flash.now[:notice] = "リクエストを送信しました。"
-      @borrow_item.update_attribute(:borrow_state_id,2)
-      render "show"
+      redirect_to @borrow_item ,notice: "リクエストを送信しました。"
     else
+      @borrow_item.borrow_state_id = 1
+      @borrowReplies = @borrow_item.borrow_replies.where(member_id: current_member)
+      @newBorrowReply = BorrowReply.new(borrow_reply_params)
       render "show"
     end
   end
@@ -30,8 +46,7 @@ class TendersController < ApplicationController
   private
   
   def borrow_reply_params
-    attrs = []
-    attrs << {borrow_replies_attributes: [:borrow_item_id,:member_id,:message,:message_state_id,:delete_flg]}
-    params.require(:borrow_item).permit(attrs)
+    attrs = [:message]
+    params.require(:borrow_item).require(:borrow_reply).permit(attrs)
   end
 end
