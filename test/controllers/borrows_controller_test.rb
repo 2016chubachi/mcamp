1cdb085313a1b082c3814c3db0476e4bf29a490a
takeshitou <takeshitou@gmail.com>
2016-07-12 01:31:34 +0900

    update test case

--- test/controllers/borrows_controller_test.rb
+++ test/controllers/borrows_controller_test.rb
@@ -1,17 +1,17 @@
 require 'test_helper'
 
 class BorrowsControllerTest < ActionController::TestCase
-  
+
   # 各テストケース前に実行
   def setup
-    
+
   end
-  
+
   # 各テストケース後に実行
   def teardown
-    
+
   end
-  
+
   #借りるアイテム一覧表示テスト
   test "should get index" do
     get :index
@@ -19,16 +19,15 @@ class BorrowsControllerTest < ActionController::TestCase
     assert_equal 20,assigns(:loanItems).count,"貸出アイテム取得件数不正"
     assert_template "index","indexテンプレートではない"
   end
-  
+
   #ログインしていないまま、借りる詳細画面表示テスト
   test "should get show is not login" do
     loanitem = loan_items(:loan_item_1)
-    get :show,{:id => loanitem.id}
-    assert_response :success,"show action 失敗しました。"
-    assert_equal 1,assigns(:loanItem).requests.count,"リクエスト件数不正"
-    assert_template "show","showテンプレートではない"
+    get :edit,{:id => loanitem.id}
+    assert_response :redirect
+    assert_redirected_to :login
   end
-  
+
   #ログインした後、借りる詳細画面表示テスト
   test "should get show is logined" do
     #user1のデータを取得
@@ -37,14 +36,14 @@ class BorrowsControllerTest < ActionController::TestCase
     log_in_as(member)
     #貸出アイテム1を取得
     loanitem = loan_items(:loan_item_1)
-    
+
     #貸出アイテム７の借りる詳細画面に遷移
-    get :show,{:id => loanitem.id}
-    assert_response :success,"show action 失敗しました。"
+    get :edit,{:id => loanitem.id}
+    assert_response :success,"edit action 失敗しました。"
     assert_equal 1,assigns(:loanItem).requests.count,"リクエスト件数不正"
-    assert_template "show","showテンプレートではない"
+    assert_template "edit","editテンプレートではない"
   end
-  
+
   #ログインしていないまま、メッセージを送信するテスト
   test "should get update is not login" do
     #アイテム６のデータ取得
@@ -53,7 +52,7 @@ class BorrowsControllerTest < ActionController::TestCase
     assert_response :redirect
     assert_redirected_to :login
   end
-  
+
   #ログインしていないまま、メッセージを送信するテスト
   test "should get update is logined" do
     #user1のデータを取得
@@ -62,28 +61,37 @@ class BorrowsControllerTest < ActionController::TestCase
     log_in_as(member)
     #アイテム６のデータ取得
     loanitem = loan_items(:loan_item_6)
-    loanitem.requests.build({:loan_item => loanitem,:member => member,
+    #関連付けのやり方
+    # loanitem.requests.build({:loan_item => loanitem,:member => member,
+    #           :message => "テストメッセージ",
+    #           :message_state => message_states(:message_state_1),
+    #           :delete_flg => false
+    # })
+    # #リクエスト送信用パラメータ作成
+    # paramsArr = loanitem.attributes
+    # requestArr= {}
+    # newRequest = nil
+    # loanitem.requests.each_with_index do |val,index|
+    #   requestArr[index.to_s] = val.attributes
+    #   newRequest = val if val.new_record?
+    # end
+    # paramsArr["requests_attributes"]=requestArr
+    #無関係のやり方
+    newRequest = Request.new({:loan_item => loanitem,:member => member,
               :message => "テストメッセージ",
               :message_state => message_states(:message_state_1),
               :delete_flg => false
     })
-    #リクエスト送信用パラメータ作成
     paramsArr = loanitem.attributes
-    requestArr= {}
-    newRequest = nil
-    loanitem.requests.each_with_index do |val,index|
-      requestArr[index.to_s] = val.attributes
-      newRequest = val if val.new_record?
-    end
-    paramsArr["requests_attributes"]=requestArr
+    paramsArr[:request] = newRequest.attributes
     #リクエスト送信
     patch :update,id: loanitem.id,loan_item: paramsArr
     assert_response :success,"update action 失敗しました。"
-    assert_template :show,"showテンプレートではない"
+    assert_template :edit,"editテンプレートではない"
     assert_equal 6,Request.all.count,"リクエスト件数不正"
     assert_equal newRequest.message,Request.last.message,"リクエスト送信失敗"
     assert_equal 2,LoanItem.find(loanitem).loan_state.id
-    
+
   end
 
 end
