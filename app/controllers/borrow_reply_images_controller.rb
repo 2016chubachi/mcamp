f30375835e84d5d992261a88db585a2c42f33c7d
takeshitou <takeshitou@gmail.com>
2016-07-20 00:39:38 +0900

    modify controller

--- app/controllers/borrow_reply_images_controller.rb
+++ app/controllers/borrow_reply_images_controller.rb
@@ -1,6 +1,8 @@
 class BorrowReplyImagesController < ApplicationController
   def show
     @image = BorrowReplyImage.find(params[:id])
-    send_data(@image.image, :type => @image.content_type, :disposition => "inline")
+    if params[:format].in?(["jpg", "png","gif"])
+      send_data(@image.image, :type => @image.content_type, :disposition => "inline")
+    end
   end
 end
